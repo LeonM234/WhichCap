0404cbf9be710f64c3eebc0b61554fe3e6ebc113
LeonM234 <LeonM234@yahoo.com>
2015-03-13 11:55:16 -0700

    Implementing WhichRepo with blank methods.

--- WhichCap/Repository/WhichRepository.cs
+++ WhichCap/Repository/WhichRepository.cs
@@ -1,11 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using WhichCap.Models;
 
 namespace WhichCap.Repository
 {
-    public class WhichRepository
+    public class WhichRepository : IWhichRepository
     {
+        private WhichDBContext _dbContext;
+
+        public int GetCount()
+        {
+            return _dbContext.Whiches.Count<Models.Which>();
+        }
+
+        public void Add(Which E)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Which E)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Which> All()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Which GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
