e06af841f4634ece6c77114c60e70b8dae8a638a
LeonM234 <LeonM234@yahoo.com>
2015-03-13 09:19:33 -0700

    WIP: Initial WhichModel structure

--- WhichCap/Models/WhichModels.cs
+++ WhichCap/Models/WhichModels.cs
@@ -7,6 +7,24 @@ namespace WhichCap.Models
 {
     public class WhichModels
     {
+        public int ID { get; set; }
 
+        // UserID here?
+
+        // Need MaxChar (or something) of 150, and min of...1?
+        // Example max case:  Which... set of earings should I buy my wife?
+        // Example min case: Which... T?  (compare two fonts)
+        public string Title { get; set; }
+
+        // Not sure if any extra parameters required
+        public string Pic1Url { get; set; }
+
+        public string Pic2Url { get; set; }
+
+        public int Vote1 { get; set; }
+
+        public int Vote2 { get; set; }
+
+        public bool Done { get; set; }
     }
 }
