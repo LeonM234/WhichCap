9a0b11b9ae52ae08b188e100a27099a35b3869bf
LeonM234 <LeonM234@yahoo.com>
2015-03-16 20:36:55 -0700

    Introduced routes.  Not currently working.

---
+++ WhichCap/Scripts/app/FeedController.js
@@ -0,0 +1,10 @@
+﻿angular.module('whichApp', ['ngRoute'])
+     .config(function ($routeProvider, /*$locationProvider*/) {
+         $routeProvider.when("/Feed", {
+             templateUrl: "/templates/feed.html", controller: "feedController"
+         });
+         $routeProvider.when("/Profile", {
+             templateUrl: "/templates/profile.html", controller: "profileController"
+         });
+         //$locationProvider.html5Mode(true);
+     });
