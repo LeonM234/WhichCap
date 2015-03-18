4e30e223c3b542c4ac71cd786dbc9b1768764405
LeonM234 <LeonM234@yahoo.com>
2015-03-18 14:48:07 -0700

    Added 'otherwise' route to which-module

--- WhichCap/Scripts/which-module.js
+++ WhichCap/Scripts/which-module.js
@@ -1,10 +1,10 @@
 ﻿var whichModule = angular.module('whichApp', ['ngRoute', 'ngResource'])
-     .config(function ($routeProvider/*$locationProvider*/) {
+     .config(function ($routeProvider) {
          $routeProvider.when("/Feed", {
              templateUrl: "/templates/feed.html", controller: "feedController"
          });
          $routeProvider.when("/Profile", {
              templateUrl: "/templates/profile.html", controller: "feedController"
          });
-         //$locationProvider.html5Mode(true);
+         $routeProvider.otherwise({ redirectTo: "/" });
      });
