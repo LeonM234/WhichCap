24458aae6a449154716ccd45742945048ed6e19e
LeonM234 <LeonM234@yahoo.com>
2015-03-19 16:22:36 -0700

    Feed order now reversed so newest on top.

--- WhichCap/Scripts/Feed/add-controller.js
+++ WhichCap/Scripts/Feed/add-controller.js
@@ -1,7 +1,6 @@
 ﻿whichModule.controller('addController', function ($scope, feedRepository, $location) {
     $scope.save = function (which) {
         feedRepository.save(which);
-        changePath();
         $location.path("Feed");
     }
 });
