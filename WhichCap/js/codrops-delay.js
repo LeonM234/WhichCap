383b3572190bf5be39f2872ab88486f42e2683ac
LeonM234 <LeonM234@yahoo.com>
2015-03-22 18:55:30 -0700

    Some initial styling.

---
+++ WhichCap/js/codrops-delay.js
@@ -0,0 +1,17 @@
+﻿var inProgress = false;
+
+Array.prototype.slice.call( document.querySelectorAll( '#la-buttons > button' ) ).forEach( function( el, i ) {
+    var anim = el.getAttribute( 'data-anim' ),
+        animEl = document.querySelector( '.' + anim );
+
+    el.addEventListener( 'click', function() {
+        if( inProgress ) return false;
+        inProgress = true;
+        classie.add( animEl, 'la-animate' );
+
+        setTimeout( function() {
+            classie.remove( animEl, 'la-animate' );
+            inProgress = false;
+        }, 6000 );
+    } );
+} );
